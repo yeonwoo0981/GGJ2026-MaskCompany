eddd9a921bb5a783081ddf157a7c020bff7d6026
yeonwoo0981 <0981syw@naver.com>
2026-01-31 01:16:30 +0900

    feat: Map system base

---
+++ Assets/Work/Yeonwoo/01_Scripts/NPC/SO/Enemy.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Work.Yeonwoo._01_Scripts.NPC.SO
+{
+    [CreateAssetMenu(fileName = "Enemy", menuName = "EnemySO/Enemy")]
+    public class Enemy : ScriptableObject
+    {
+        
+    }
+}
