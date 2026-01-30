24531b2c9b63841c0218790839ee32be74265fbf
yeonwoo0981 <0981syw@naver.com>
2026-01-31 03:46:48 +0900

    feat: Pooling

--- Assets/Work/Yeonwoo/01_Scripts/Map/MapManager.cs
+++ Assets/Work/Yeonwoo/01_Scripts/Map/MapManager.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
+using Work.Yeonwoo._01_Scripts.Map.SO;
 
 namespace Work.Yeonwoo._01_Scripts.Map
 {
     public class MapManager : MonoBehaviour
     {
-        
+        [SerializeField] private MapList mapListSO;
     }
 }
