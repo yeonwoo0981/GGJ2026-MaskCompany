b5312e15df71cac656a23dbb2f461e2c486e61d9
yeonwoo0981 <0981syw@naver.com>
2026-01-31 13:37:50 +0900

    feat: NPC

--- Assets/Work/Yeonwoo/01_Scripts/NPC/Character/NPC.cs
+++ Assets/Work/Yeonwoo/01_Scripts/NPC/Character/NPC.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using Work.Yeonwoo._01_Scripts.NPC.SO;
 
 namespace Work.Yeonwoo._01_Scripts.NPC.Character
@@ -6,5 +7,13 @@ namespace Work.Yeonwoo._01_Scripts.NPC.Character
     public abstract class NPC : MonoBehaviour
     {
         [SerializeField] protected Enemy typeSO;
+        
+        protected virtual void Update()
+        {
+            if (typeSO.AwarenessScore < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- Assets/Work/Yeonwoo/01_Scripts/NPC/SO/Enemy.cs
+++ Assets/Work/Yeonwoo/01_Scripts/NPC/SO/Enemy.cs
@@ -17,12 +17,26 @@ namespace Work.Yeonwoo._01_Scripts.NPC.SO
         Angry,
         Fear
     }
+
+    public enum SensingType
+    {
+        Wall,
+        Sound,
+        None
+    }
+    
     [CreateAssetMenu(fileName = "Enemy", menuName = "EnemySO/Enemy")]
     public class Enemy : ScriptableObject
     {
+        [SerializeField] private string descriptionName;
         [field:SerializeField] public MaskType MaskType { get; private set; }
         [field:SerializeField] public FeelType FeelType { get; private set; }
-        [field:SerializeField] public float AwarenessScore { get; private set; }
-        [field:SerializeField] public float MyManScore { get; private set; }
+        [field:SerializeField] [field:Range(1, 1000)] public float AwarenessScore { get; private set; }
+        [field:SerializeField] [field:Range(1, 100)] public float MyManScore { get; private set; }
+        [field: SerializeField] public float Speed { get; private set; }
+        [field:SerializeField] public float RotationSpeed { get; private set; }
+        [field:SerializeField] public SensingType SensingType { get; private set; }
+        [field:SerializeField] public float SensingRange { get; private set; }
+        [field:SerializeField] public int Angle { get; private set; }
     }
 }
